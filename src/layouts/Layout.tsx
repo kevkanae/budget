1d42db72364d855c810e393dfbc305aebec2b2d9
Kevin D Goveas <kevin.goveas@cognitiveclouds.com>
2023-03-25 17:25:49 +0530

    feat: add check for core file exist or not

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,10 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet, useNavigate } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Sidebar from "../components/Sidebar/Sidebar";
 import Box from "@mui/material/Box";
 import { SxProps } from "@mui/material/styles";
+import { exists, BaseDirectory } from "@tauri-apps/api/fs";
+import { useState, useEffect, useCallback } from "react";
 
 const Layout = () => {
+  const [indexFileExists, setIndexFileExists] = useState<boolean>(false);
+
   const root: SxProps = {
     height: "100vh",
     width: "100%",
@@ -21,12 +25,23 @@ const Layout = () => {
     bgcolor: "background.default",
   };
 
+  const checkUser = useCallback(async () => {
+    const isExistingUser = await exists("index.json", {
+      dir: BaseDirectory.AppLocalData,
+    });
+    setIndexFileExists(isExistingUser);
+  }, []);
+
+  useEffect(() => {
+    checkUser();
+  }, [checkUser]);
+
   return (
     <Box sx={root}>
       <Header />
       <Box sx={main}>
         <Sidebar />
-        <Outlet />
+        {indexFileExists ? <Outlet /> : <Navigate to="/" />}
       </Box>
     </Box>
   );
